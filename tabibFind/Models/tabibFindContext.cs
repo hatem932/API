848dbc77b274d463c79f03072ddf825d71aaafaa
hatem932 <37874744+hatem932@users.noreply.github.com>
2021-02-15 22:26:33 +0200

    Add files via upload

---
+++ tabibFind/Models/tabibFindContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tabibFind.Models
+{
+    public class tabibFindContext : DbContext
+    {
+        public tabibFindContext(DbContextOptions<tabibFindContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<tabibFind> TodoItems { get; set; }
+    }
+
+    }
+}
